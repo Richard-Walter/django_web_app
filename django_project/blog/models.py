9082b2404b9d4c5128d0f08ea4376f405f15a082
Richard-Walter <willthisnamedo@gmail.com>
2019-06-04 17:45:17 +1000

    create user and blog post

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models (Databases) here.
+
+# Each class is going to be its own table in the database
+# Advantage of this - you need to no SQL to create the Post database
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  #delete posts if the user is deleted
+
